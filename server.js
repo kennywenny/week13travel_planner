88f72a0117ee41a39a3eadef306070f22d986ae8
Kenny Hung (he) <khung@instructors.2u.com>
2022-12-19 20:30:45 +0000

    Connection to the database

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 require('dotenv').config()
 const express = require('express')
+const sequelize = require('./db/connection')
 
 const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
+sequelize.sync({ force: false }).then(() => {
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+  })
 })
