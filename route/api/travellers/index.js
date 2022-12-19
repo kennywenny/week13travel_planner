c90fa54b5011bb65c7e6696f3207cfe4252db5d6
Kenny Hung (he) <khung@instructors.2u.com>
2022-12-19 21:09:29 +0000

    GET /api/travellers

--- route/api/travellers/index.js
+++ route/api/travellers/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 const Traveller = require('../../../model/traveller')
 
+router.get('/', async (_, res) => {
+  const results = await Traveller.findAll()
+  res.json(results)
+})
+
 router.post('/', async (req, res) => {
   await Traveller.create(req.body)
   res.status(204).end()
