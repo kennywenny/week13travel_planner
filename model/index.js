1110138291bbd3f0cc38efabc7d17acbfa5840cd
Kenny Hung (he) <khung@instructors.2u.com>
2022-12-19 20:49:27 +0000

    Set up relations in models

--- model/index.js
+++ model/index.js
@@ -2,4 +2,16 @@ const Traveller = require('./traveller')
 const Location = require('./location')
 const Trip = require('./trip')
 
+Traveller.belongsToMany(Location, {
+  through: "trip",
+  as: "locations",
+  foreignKey: "traveller_id"
+});
+
+Location.belongsToMany(Traveller,{
+  through: "trip",
+  as: "travellers",
+  foreignKey: "location_id"
+})
+
 module.exports = { Traveller, Location, Trip }
